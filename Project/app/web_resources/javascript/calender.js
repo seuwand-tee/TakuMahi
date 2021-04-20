91cf8ba68f09d94c9125f8708bef7221d41e0c61
Leo Ducrot <ducle591@student.otago.ac.nz>
2021-04-20 22:22:22 +1200

    Cleanned up the table and added new views as well as some buttons for later

--- Project/app/web_resources/javascript/calender.js
+++ Project/app/web_resources/javascript/calender.js
@@ -1,11 +1,44 @@
-/* selects the cell that the mouse is in */
-$("#calander td").on("mouseover", function(){
+/* hide initially unsued calender views. */
+
+/* selects the cell that the mouse is in. */
+$(".calender td").on("mouseover", function(){
   $(this).addClass("selected");
-  console.log("working")
 });
 
-/* deselects the cell the mouse is in */
-$("#calander td").on("mouseout", function(){
+/* deselects the cell the mouse is in. */
+$(".calender td").on("mouseout", function(){
   $(this).removeClass("selected");
-  console.log("working")
+});
+
+/* what happens when next button is clicked */
+$("#next-button").on("click", function(){
+  alert("Not supported yet!");
+});
+
+/* what happens when previous button is clicked */
+$("#previous-button").on("click", function(){
+  alert("Not supported yet!");
+});
+
+/* Check which calender view is enabled. */
+/* setup listenrs for buttons. */
+$("#day").on("click", function(){
+  /* hide all other tables besdies the selected one. */
+  $("#calender-week").addClass("hidden-calender");
+  $("#calender-month").addClass("hidden-calender");
+  $("#calender-day").removeClass("hidden-calender");
+});
+
+$("#week").on("click", function(){
+  $("#calender-week").removeClass("hidden-calender");
+  $("#calender-month").addClass("hidden-calender");
+  $("#calender-day").addClass("hidden-calender");
+});
+
+$("#month").on("click", function(){
+  $("#calender-week").addClass("hidden-calender");
+  $("#calender-month").removeClass("hidden-calender");
+  $("#calender-day").addClass("hidden-calender");
+  /* TODO */
+  alert("Not yet implemented");
 });
