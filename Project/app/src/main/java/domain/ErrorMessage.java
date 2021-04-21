f837c4d02d383eea1610a5c3a38a2398495cc4ee
Alec <belki240@student.otago.ac.nz>
2021-04-21 23:51:12 +1200

    Added User Resources and updated Server class

---
+++ Project/app/src/main/java/domain/ErrorMessage.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class ErrorMessage {
+
+	private String message;
+
+	public ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
--- Project/app/src/main/java/service/Server.java
+++ Project/app/src/main/java/service/Server.java
@@ -1,20 +1,26 @@
 package service;
 
+import dao.LocalStorageDAO;
+import resource.UserListResource;
 import org.jooby.Jooby;
 import org.jooby.json.Gzon;
+import org.jooby.handlers.Cors;
+import org.jooby.handlers.CorsHandler;
 import java.io.IOException;
 
 public class Server extends Jooby {
 
     public Server() {
 
-        // Create new DAO instance here
+        LocalStorageDAO dao = new LocalStorageDAO();
 
         port(8080);
-
+        
+        use("*", new CorsHandler(new Cors().withMethods("*")));
+        
         use(new Gzon());
 
-        // use(new <Resource>(<DAO>));
+        use(new UserListResource(dao));
 
     }
 
