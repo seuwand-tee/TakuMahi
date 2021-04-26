d48182ee81df3cbcdfa21fe29ce4afaee39ce2ec
Kieran <belki240@student.otago.ac.nz>
2021-04-26 13:11:36 +1200

    Implemented remove user from shift feature

--- Project/app/src/main/java/resource/OpenShiftWithUserResource.java
+++ Project/app/src/main/java/resource/OpenShiftWithUserResource.java
@@ -2,6 +2,8 @@ package resource;
 
 import dao.LocalStorageDAO;
 import domain.ErrorMessage;
+import domain.Event;
+import domain.User;
 import org.jooby.Jooby;
 import org.jooby.MediaType;
 import org.jooby.Status;
@@ -18,6 +20,7 @@ public class OpenShiftWithUserResource extends Jooby {
 				Integer eventId = Integer.parseInt(req.param("eventId").value());
                                 Integer userId = Integer.parseInt(req.param("userId").value());
                                 
+                                //checks Event and User exist
                                 if ((dao.eventExists(eventId))&&(dao.userExists(userId))) {
 					// IDs are OK, so pass request on to DAO
 					dao.assignShiftToUser(userId, eventId);
@@ -30,17 +33,29 @@ public class OpenShiftWithUserResource extends Jooby {
 			});
                         
                         //Removes user from shift
-//			put("/:id", (req, rsp) -> {
-//				String id = req.param("id").value();
-//				Account account = req.body().to(Account.class);
-//
-//				if (!id.equals(account.getId())) {
-//					rsp.status(Status.CONFLICT).send(new ErrorMessage("Modifying the Customer ID via this operation is not allowed."));
-//				} else {
-//                                        dao.updateAccount(id, account);
-//					rsp.status(Status.NO_CONTENT);
-//				}
-//			});
+			put("/:eventId/:userId", (req, rsp) -> {
+				//extracts params
+				Integer eventId = Integer.parseInt(req.param("eventId").value());
+                                Integer userId = Integer.parseInt(req.param("userId").value());
+                                
+                                //checks event and user exists
+                                if ((dao.eventExists(eventId))&&(dao.userExists(userId))) {
+                                    Event event = dao.getEventByID(eventId);
+                                    User user = dao.getUserByID(userId);
+                                    //ids are ok, check user matches shift
+                                    if (event.getUser()==user) {
+                                        //event matches user, delete user from shift
+                                        dao.removeShiftFromUser(userId, eventId);
+                                        rsp.status(Status.NO_CONTENT);
+                                    } else {
+                                        rsp.status(Status.NOT_FOUND).send(new ErrorMessage("The user specified is not assigned to shift"));
+                                    }
+                                } else {
+                                    // Bad ID - send a 404 response
+                                    rsp.status(Status.NOT_FOUND).send(new ErrorMessage("Invalid Event or User ID."));
+                                }
+                                
+			});
             
         }).produces(MediaType.json).consumes(MediaType.json);
     }
