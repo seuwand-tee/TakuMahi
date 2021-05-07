564f2b69ceac489f6e466c26990e36f8180fe7ff
Leo Ducrot <ducle591@student.otago.ac.nz>
2021-05-07 13:31:15 +1200

    Added code that will duplicate the shifts view for the number of staff

--- Project/app/Web Resources/js/shift-manage.js
+++ Project/app/Web Resources/js/shift-manage.js
@@ -1,4 +1,27 @@
-/* hide initially unsued calendar views. */
+var usrdata;
+
+/* Creates a schedule for each user pulled and fills in the data */
+function populate(){
+  console.log(usrdata[0]);
+  // Duplicate the weekly view and create a new one for each element.
+  usrdata.forEach((element, index) => {
+    $("#calendar-day").clone().attr("id", "calendar-day" + index).appendTo("body");
+    console.log("123");
+  });
+  $('th:contains("User")').text(usrdata[0].firstName);
+}
+
+/* pull in the data */
+fetch('http://localhost:8080/api/staff')
+  .then(response => response.json())
+  .then(data => {
+    usrdata = data;
+    populate();
+
+  });
+
+/* update the first entry */
+// $('th:contains("user")').text(usrdata[0].idNumber);
 
 /* selects the cell that the mouse is in. */
 $(".calendar td").on("mouseover", function(){
@@ -12,6 +35,11 @@ $(".calendar td").on("mouseout", function(){
 
 /* what happens when next button is clicked */
 $("#next-button").on("click", function(){
+  alert("Not supported yet!");
+});
+
+/* what happens when save button is clicked */
+$("#save-button").on("click", function(){
   alert("Not supported yet!");
 });
 
