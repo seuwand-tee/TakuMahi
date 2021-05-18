be51eb1ca929c98c50e2139b56e02bea5b6f6f43
User <User@LAPTOP-B7R0KD45.student.uod.otago.ac.nz.>
2021-05-18 18:55:10 +1200

    Schema done

--- Project/app/src/main/java/dao/schema.sql
+++ Project/app/src/main/java/dao/schema.sql
@@ -8,31 +8,25 @@ lastName varchar(100) not null,
 emailaddress varchar(100) not null,
 constraint User_PK primary key (idnumber)
 );
-create table Event(
-eventid int(10) not null unique,
-idnumber int(10) not null,
-start SMALLDATETIMEFROMPARTS not null,
-end SMALLDATETIMEFROMPARTS not null,
-constraint Event_PK primary key (eventid),
-constraint Event_User_FK foreign key (idnumber) references User(idnumber)
-);
+
 create table Shift(
-eventid int(10) not null,
-start SMALLDATETIMEFROMPARTS not null,
-end SMALLDATETIMEFROMPARTS not null,
-name varchar(100) not null,
+shiftid int(10) not null,
+start DATETIME not null,
+end DATETIME not null,
 description varchar(500) not null,
 notes varchar(500),
 type varchar(100),
-constraint Shift_PK primary key (sale_id),
-constraint Shift_Customer_FK foreign key (customer_id) references Customer(customer_id)
+constraint Shift_PK primary key (shiftid)
 );
-create table SaleItem(
-quantityPurchased decimal(6,2) not null,
-salePrice decimal(6,2) not null,
-sale_id int(10) not null,
-productID varchar(50) not null,
-constraint SaleItem_PK primary key (sale_id, productID),
-constraint SaleItem_Sale_FK foreign key (sale_id) references Sale(sale_id),
-constraint SaleItem_Product_FK foreign key (ProductID) references Product(ProductID)
+
+create table Unavailabilty(
+shiftid int(10) not null ,
+idnumber int(10) not null,
+start DATETIME not null,
+end DATETIME not null,
+constraint Unavailability_PK primary key (shiftid, idnumber),
+constraint Unavailability_User_FK foreign key (idnumber) references User(idnumber),
+constraint Unavailability_Shift_FK foreign key (shiftid) references Shift(shiftid)
 );
+
+
